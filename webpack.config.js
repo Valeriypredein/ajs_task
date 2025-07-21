00927a77159ed77de620a3081014dc0ec6c80055
valeriypredein <valeriypredein@gmail.com>
2025-07-22 00:14:40 +0300

    ajs_1

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'ajs',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+           
+            presets: [
+              ['@babel/preset-env', {
+                modules: 'commonjs'  
+              }]
+            ],
+            plugins: [
+              '@babel/plugin-transform-modules-commonjs' 
+            ]
+          }
+        },
+      },
+    ],
+  },
+};
